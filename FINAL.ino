7a73ac17069cebc24a4578202d8c7f47433c52e6
Nick <nbeiner20@gmail.com>
2025-12-02 16:11:34 -0500

    wrote a version of buttonRead that is likely incorrect or inefficient. wrote gyroLVL function and implemented reading of gyro data by using a struct. made lightLVL into just a "getter" function. Fixed if statements for buttonRead to check for LOW instead of HIGH. initialized pins for buttons/lights and mpu for the gyro

--- FINAL.ino
+++ FINAL.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#define B1 
+#define B1
 #define B2
 #define B3
 #define B4
@@ -10,6 +10,7 @@
 #define LED2
 #define LED3
 #define LED4
+#define LIGHT_PIN
 
 // default timer settings
 int hours = 8;
@@ -38,6 +39,17 @@ bool light = true;
 
 Adafruit_MPU6050 mpu;
 SSD1306Wire lcd(0x3c, SDA, SCL);
+
+struct GyroData {
+  float gx;
+  float gy;
+  float gz;
+  float ax;
+  float ay;
+  float az;
+  float temperature;
+};
+
 void setup() {
   lcd.init();
   lcd.flipScreenVertically();
@@ -46,10 +58,39 @@ void setup() {
   String text = "00:00:00";
   lcd.drawString(0, 0, text);
   lcd.display(); 
+
+  if (!mpu.begin()) {
+    lcd.clear();
+    lcd.drawString(0,0,"MPU FAIL");
+    lcd.display();
+  }
+  pinMode(B1, INPUT_PULLUP);
+  pinMode(B2, INPUT_PULLUP);
+  pinMode(B3, INPUT_PULLUP);
+  pinMode(B4, INPUT_PULLUP);
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+
+  pinMode(LIGHT_PIN, INPUT);
 }
 
 int buttonRead(int button){
-  
+  const unsigned long DEBOUNCE_MS = 40;
+  if (digitalRead(button) == LOW) {
+    unsigned long start = millis();
+    while (millis() - start < DEBOUNCE_MS) {
+      if (digitalRead(button) == HIGH) return HIGH;
+      delay(1);
+    }
+    while (digitalRead(button) == LOW) {
+      delay(10);
+    }
+    return LOW;
+  }
+  return HIGH;
 }
 
 void timer(unsigned long time){
@@ -72,30 +113,33 @@ void timer(unsigned long time){
 }
 
 void set_settings(){
-  int screenCycle = 0;
-  if (buttonRead(B1) == HIGH){
+  static int screenCycle = 0;
+  if (buttonRead(B1) == LOW){
     // cycle screen to previous setting
     screenCycle -= 1;
     if (screenCycle < 0){
       screenCycle = 0;
     }
   }
-  if (buttonRead(B4) == HIGH){
+  if (buttonRead(B4) == LOW){
     // cycle screen to next setting
     screenCycle += 1;
+    if (screenCycle > 7){
+      return;
+      // end of settings
+    }
   }
-
 
   if (screenCycle == 0){
     // set hours
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // increase
       hours += 1;
       if (hours > 23){
         hours = 0;
       }
     }
-    if (buttonRead(B3) == HIGH){
+    if (buttonRead(B3) == LOW){
       // decrease
       hours -= 1;
       if (hours < 0){
@@ -105,14 +149,14 @@ void set_settings(){
   }
   else if (screenCycle == 1){
     // set minutes
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // increase
       minutes += 1;
       if (minutes > 59){
         minutes = 0;
       }
     }
-    if (buttonRead(B3) == HIGH){
+    if (buttonRead(B3) == LOW){
       // decrease
       minutes -= 1;
       if (minutes < 0){
@@ -122,14 +166,14 @@ void set_settings(){
   }
   else if (screenCycle == 2){
     // set seconds
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // increase
       seconds += 1;
       if (seconds > 59){
         seconds = 0;
       }
     }
-    if (buttonRead(B3) == HIGH){
+    if (buttonRead(B3) == LOW){
       // decrease
       seconds -= 1;
       if (seconds < 0){
@@ -139,47 +183,57 @@ void set_settings(){
   }
   else if (screenCycle == 3){
     // set alarm
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // toggle alarm
       alarm = !alarm;
     }
   }
   else if (screenCycle == 4){
     // set buzzer
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // toggle buzzer
       buzzer = !buzzer;
     }
   }
   else if (screenCycle == 5){
     // set shaking
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // toggle shaking
       shake = !shake;
     }
   }
   else if (screenCycle == 6){
     // set game
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // toggle game
       game = !game;
     }
   }
   else if (screenCycle == 7){
     // set light sensor
-    if (buttonRead(B2) == HIGH){
+    if (buttonRead(B2) == LOW){
       // toggle light sensor
       light = !light;
     }
   }
 }
 
-int lightLVL(bool setting){
-  if (setting == true){
-    // read light sensor value
-    int sensorValue = analogRead();
-    return sensorValue;
-  }
+int lightLVL(){
+  int sensorValue = analogRead(LIGHT_PIN);
+  return sensorValue;
+}
+
+GyroData gyroLVL(){
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  GyroData d;
+  d.gx = g.gyro.x; // rad/s
+  d.gy = g.gyro.y;
+  d.gz = g.gyro.z;
+  d.ax = a.acceleration.x; // m/s^2
+  d.ay = a.acceleration.y;
+  d.az = a.acceleration.z;
+  return d;
 }
 
 void loop() {
